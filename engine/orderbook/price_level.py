218a109dc31344f261bfa2137b95d289a4df166b
LucioJamieAI <148231849+JadoreThompson@users.noreply.github.com>
2025-07-09 09:01:05 +0100

    Encapsulated PriceLevel properties

--- engine/orderbook/price_level.py
+++ engine/orderbook/price_level.py
@@ -4,30 +4,30 @@ from ..order import Order
 
 class PriceLevel:
     def __init__(self) -> None:
-        self.head: PriceLevelNode | None = None
-        self.tail: PriceLevelNode | None = None
-        self.tracker: dict[str, PriceLevelNode] = {}
+        self._head: PriceLevelNode | None = None
+        self._tail: PriceLevelNode | None = None
+        self._tracker: dict[str, PriceLevelNode] = {}
 
     def append(self, order: Order) -> None:
-        if order.payload["order_id"] in self.tracker:
+        if order.payload["order_id"] in self._tracker:
             raise ValueError(
                 f"Order with id {order.payload['order_id']} already on level."
             )
 
         new_node = PriceLevelNode(order)
 
-        if self.head is None:
-            self.head = new_node
-            self.tail = new_node
+        if self._head is None:
+            self._head = new_node
+            self._tail = new_node
         else:
-            self.tail.next = new_node
-            new_node.prev = self.tail
-            self.tail = new_node
+            self._tail.next = new_node
+            new_node.prev = self._tail
+            self._tail = new_node
 
-        self.tracker[order.payload["order_id"]] = new_node
+        self._tracker[order.payload["order_id"]] = new_node
 
     def remove(self, order: Order) -> None:
-        orders_node = self.tracker[order.payload["order_id"]]
+        orders_node = self._tracker[order.payload["order_id"]]
 
         if orders_node.prev is not None:
             orders_node.prev.next = orders_node.next
@@ -35,16 +35,28 @@ class PriceLevel:
         if orders_node.next is not None:
             orders_node.next.prev = orders_node.prev
 
-        if self.head == orders_node:
-            self.head = orders_node.next
+        if self._head == orders_node:
+            self._head = orders_node.next
 
-        if self.tail == orders_node:
-            self.tail = orders_node.prev
+        if self._tail == orders_node:
+            self._tail = orders_node.prev
 
         # Cleanup
         orders_node.prev = None
         orders_node.next = None
-        self.tracker.pop(order.payload["order_id"])
+        self._tracker.pop(order.payload["order_id"])
 
     def __bool__(self):
-        return self.head is not None
+        return self._head is not None
+
+    @property
+    def head(self) -> PriceLevelNode | None:
+        return self._head
+
+    @property
+    def tail(self) -> PriceLevelNode | None:
+        return self._tail
+
+    @property
+    def tracker(self) -> dict[str, PriceLevelNode]:
+        return self._tracker
